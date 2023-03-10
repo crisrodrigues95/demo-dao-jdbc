f5415f35815237c9d2c9d340c220fd7904bcec6a
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-03-10 11:22:34 -0300

    Department entity class

--- src/aplications/Programm.java
+++ src/aplications/Programm.java
@@ -1,14 +1,19 @@
 package aplications;
 
+import java.util.Date;
+
 import model.entities.Department;
+import model.entities.Seller;
 
 public class Programm {
 
 	public static void main(String[] args) {
 		
-		Department dp = new Department(1, "Cristhian");
+		Department dp = new Department(1, "Bomboniere");
 		
-		System.out.println(dp);
+		Seller seller = new Seller(21, "Bob", "bob@gmail.com", new Date(), 3000.0, dp);
+		
+		System.out.println(seller);
 
 	}
 
