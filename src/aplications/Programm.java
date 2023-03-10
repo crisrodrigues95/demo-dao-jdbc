e5c9adada3923cbef181d9a4ff3892701527dbf9
Cristhian Rodrigues <cristhian.rodrigues95@gmail.com>
2023-03-10 11:10:31 -0300

    Department entity class

---
+++ src/aplications/Programm.java
@@ -0,0 +1,15 @@
+package aplications;
+
+import model.entities.Department;
+
+public class Programm {
+
+	public static void main(String[] args) {
+		
+		Department dp = new Department(1, "Cristhian");
+		
+		System.out.println(dp);
+
+	}
+
+}
